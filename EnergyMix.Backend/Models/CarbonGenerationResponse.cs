fd1be69847d2720cf7dac943a7216e35aef57bf3
Wacob Jitek <McWicio@gmail.com>
2026-06-11 01:00:47 +0200

    Changes

---
+++ EnergyMix.Backend/Models/CarbonGenerationResponse.cs
@@ -0,0 +1,10 @@
+﻿using EnergyMix.Backend.Models;
+using System.Text.Json.Serialization;
+
+namespace Energy.Mix.Backend.Models;
+public class CarbonGenerationResponse
+{
+    [JsonPropertyName("data")]
+    public List<Generationinterval> Data { get; init; } = [];
+
+}
---
+++ EnergyMix.Backend/Models/GenerationInterval.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EnergyMix.Backend.Models;
+
+public class Generationinterval
+{
+    [JsonPropertyName("from")]
+    public DateTimeOffset From { get; init; }
+
+    [JsonPropertyName("to")]
+    public DateTimeOffset To { get; init; }
+
+    [JsonPropertyName("generationmix")]
+    public List<GenerationMixItem> GenerationMix { get; init; } = [];
+
+}
